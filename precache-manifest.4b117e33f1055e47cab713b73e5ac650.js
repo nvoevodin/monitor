0590ef165ef58d5e879e1eedb4f955b6173f799f
nvoevodin <46409820+nvoevodin@users.noreply.github.com>
2019-09-15 13:32:16 -0400

    Add files via upload
    a

---
+++ precache-manifest.4b117e33f1055e47cab713b73e5ac650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf2da8ec0bb80ee5538",
+    "url": "/static/css/main.232dd9fe.chunk.css"
+  },
+  {
+    "revision": "dcf2da8ec0bb80ee5538",
+    "url": "/static/js/main.0d1f51db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "185755a00afc0f7113d8",
+    "url": "/static/js/2.9f092297.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "142d6485c36a9979aa764961e94d259d",
+    "url": "/index.html"
+  }
+];
